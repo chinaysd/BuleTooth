beece8960080924abec8aef1357a40ff90ba3f47
ysd <ysd@emomo.com>
2019-03-25 16:10:11 +0800

    [UART1]first commit

--- ac69_sdk_v2012_p2_SI_V1_C245_开发板配套测试程序_全功能_上电串口打印输出PC2/include_lib/encode/mp2_encode_api.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MP2_ENCODE_API_H_
-#define _MP2_ENCODE_API_H_
-
-#include "typedef.h"
-
-#define ENCODE_MP2_EN       1
-
-#endif
--- ac69_sdk_v2012_p2_SI_V1_C245_开发板配套测试程序_全功能_上电串口打印输出PC2/include_lib/font/font_utf16toansi.h
+++
@@ -1,8 +0,0 @@
-#ifndef __UTF16TOANSI_H__
-#define __UTF16TOANSI_H__
-
-#include "typedef.h"
-
-u16 utf16tocountryGB(u16 utf);
-
-#endif
